2e2f4992c1552e20abc4b8cf01dd3a47dc9fa7cf
Selin Huang <selin.hyx@gmail.com>
2019-08-26 08:24:06 -0400

    attempt to solve signup form empty field error

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,21 +1,23 @@
 class UsersController < ApplicationController
   def new
+    user = User.new
   end
 
   def create
-    user = User.create(user_params)
+    user = User.new(user_params)
     if user.save
       session[:user_id] = user.id
       redirect_to '/'
     else
       redirect_to '/signup'
+      # render '/signup'
     end
   end
 
   private
 
   def user_params
-    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
+    params.require(:users).permit(:first_name, :last_name, :email, :password, :password_confirmation)
   end
 
 end
